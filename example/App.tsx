6e6f55235d0ab83ca2837e2c12d2cca5463e9ecc
Jonathan Sexton <joncsexton@gmail.com>
2024-12-04 14:24:59 -0600

    initial project scaffolding

--- example/App.tsx
+++ example/App.tsx
@@ -1,39 +1,48 @@
+/* eslint-disable prettier/prettier */
 import { StyleSheet, View } from 'react-native';
-import {AdvancedImage, AdvancedVideo} from 'cloudinary-react-native';
-import {Cloudinary} from '@cloudinary/url-gen';
-import {scale} from "@cloudinary/url-gen/actions/resize";
-import {cartoonify} from "@cloudinary/url-gen/actions/effect";
-import {max} from "@cloudinary/url-gen/actions/roundCorners";
-import React, {useRef} from "react";
+import { AdvancedImage, AdvancedVideo } from 'cloudinary-react-native';
+import { Cloudinary } from '@cloudinary/url-gen';
+import { scale } from '@cloudinary/url-gen/actions/resize';
+import { cartoonify } from '@cloudinary/url-gen/actions/effect';
+import { max } from '@cloudinary/url-gen/actions/roundCorners';
+import React, { useRef } from 'react';
 import { streamingProfile } from '@cloudinary/url-gen/actions/transcode';
 import { Video } from 'expo-av';
-import {  } from 'cloudinary-react-native';
+import {} from 'cloudinary-react-native';
 
 const cld = new Cloudinary({
   cloud: {
-    cloudName: 'demo'
+    cloudName: 'demo',
   },
   url: {
-    secure: true
-  }
+    secure: true,
+  },
 });
 export default function App() {
-
   const videoPlayer = useRef<Video>(null);
   function createMyImage() {
-    var myImage = cld.image('sample').resize(scale().width(300)).effect(cartoonify()).roundCorners(max());
-    return myImage
+    var myImage = cld
+      .image('sample')
+      .resize(scale().width(300))
+      .effect(cartoonify())
+      .roundCorners(max());
+    return myImage;
   }
 
   function createMyVideoObject() {
-    const myVideo = cld.video('sea_turtle.m3u8').transcode(streamingProfile("auto"))
-    return myVideo
-  };
+    const myVideo = cld
+      .video('sea_turtle.m3u8')
+      .transcode(streamingProfile('auto'));
+    return myVideo;
+  }
 
   return (
     <View style={styles.container}>
       <View>
-        <AdvancedImage cldImg={createMyImage()} style={{backgroundColor:"black", width:300, height:200}}/>
+        <AdvancedImage
+          cldImg={createMyImage()}
+          style={{ backgroundColor: 'black', width: 300, height: 200 }}
+        />
       </View>
       <View style={styles.videoContainer}>
         <AdvancedVideo
