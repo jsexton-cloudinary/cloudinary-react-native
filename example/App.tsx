86d377620aa48340c828fbbbeb7523f6f3449c48
Jonathan Sexton <joncsexton@gmail.com>
2024-12-06 07:55:11 -0600

    video chapters example code

--- example/App.tsx
+++ example/App.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import { StyleSheet, View } from 'react-native';
-import { AdvancedImage, AdvancedVideo } from 'cloudinary-react-native';
+import { AdvancedImage, AdvancedVideo, } from 'cloudinary-react-native';
 import { Cloudinary } from '@cloudinary/url-gen';
 import { scale } from '@cloudinary/url-gen/actions/resize';
 import { cartoonify } from '@cloudinary/url-gen/actions/effect';
@@ -168,6 +168,39 @@ export default function App() {
     .transcode(streamingProfile('auto'));
     return myVideo;
   }
+  /* VIDEO CHAPTERS */
+  // Replace with your VTT file URL
+  // const vttUrl = 'https://res.cloudinary.com/YOUR_CLOUD_NAME/raw/upload/vtt/YOUR_VTT_FILE.vtt';
+  // const jumpToChapter = (startTime:any) => {
+  //   if (videoPlayer.current) {
+  //     videoPlayer.current.seek(startTime);
+  //   }
+  // };
+
+  // <View>
+  // <AdvancedVideo
+  //       ref={videoPlayer}
+  //       cldVideo={createMyVideoObject()}
+  //       videoStyle={styles.video}
+  //       controls={true}
+  //       textTracks={[
+  //         {
+  //           kind: 'subtitles',
+  //           src: vttUrl,
+  //           srcLang: 'en',
+  //           label: 'English',
+  //           default: true,
+  //         },
+  //       ]}
+  //     />
+      {/* Add your styles under the chapterButtons heading below */}
+      {/* <View style={styles.chapterButtons}>
+        <Button title="Chapter 1" onPress={() => jumpToChapter(0)} />
+        <Button title="Chapter 2" onPress={() => jumpToChapter(30)} />
+        <Button title="Chapter 3" onPress={() => jumpToChapter(60)} />
+      </View>
+      </View> */}
+  /* VIDEO CHAPTER */
   
   return (
     <View style={styles.container}>
